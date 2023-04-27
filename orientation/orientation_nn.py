5cce9904184cbd0a35b747f3026eb340c0c4ca44
myrriad <myrrhyad@gmail.com>
2023-04-27 13:31:21 -0500

    Use pretrained densenet to achieve 98.8% validation acc

--- orientation/orientation_nn.py
+++ orientation/orientation_nn.py
@@ -1,5 +1,8 @@
 import glob
+import math
 
+import PIL
+import numpy as np
 from torch import nn
 from torch.nn import Module
 import torch.nn.functional as F
@@ -35,8 +38,8 @@ class OrientationImageFolder(ImageFolder):
     #@override
     def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
         # customize to only use one class
-        classes = ['upright', 'upside_down']
-        class_to_idx = {'upright': 0, 'upside_down': 1}
+        classes = ['normal', 'flipped']
+        class_to_idx = {'normal': 0, 'flipped': 1}
         return classes, class_to_idx
 
     # TODO: This can be overridden (make_dataset) to e.g. read files from a compressed zip file instead of from the disk.
@@ -90,7 +93,11 @@ class OrientationImageFolder(ImageFolder):
         if target == 1:
             # torchvision.transforms.RandomRotation((180, 180), expand=False)
             # sample = transform180(sample)
+
             sample = torchvision.transforms.functional.rotate(sample, 180, expand=False)
+
+            # i'm desperate. just invert the whole thing. surely the cnn can detect that, right?
+            # sample = torchvision.transforms.functional.invert(sample)
                 # vflip(sample)
 
         if self.transform is not None:
@@ -107,29 +114,55 @@ class OrientationCNN(Module):
     def __init__(self):
         super(OrientationCNN, self).__init__()
         # 3 = channel
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(squish_to_linear, 128) # 128 * 28 * 28, 128) # 28 = 224 / 2 / 2 / 2
         # 65536 = 128 * (32 / 2**3) * (256 / 2**3) * 4 (batch)
-        self.fc2 = nn.Linear(128, 2)
-        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2) # oh good, we do a bit of pooling
         x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
+        x = F.max_pool2d(x, 2) # kernel_size = stride = 2
         x = F.relu(self.conv3(x))
         x = F.max_pool2d(x, 2)
         x = x.view(-1, squish_to_linear) # 128 * 28 * 28)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
         x = self.dropout(x)
-        # x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
         return x
 
+class PadToSize:
+    def __init__(self, size, pad_with=0, center=False):
+        self.size = size
+        self.pad_with = pad_with
+        self.center = center
+
+    def __call__(self, img):
+        # fill with white
+        # if img is bigger than size, crop it
+        # if img is smaller than size, pad it
+
+        # if img.size[0] > self.size[0] or img.size[1] > self.size[1]:
+        #     img = torchvision.transforms.functional.crop(img, 0, 0, self.size[0], self.size[1])
+        # if self.center:
+        vertmargin = max(self.size[0] - img.size[0], 0) / 2
+        horizmargin = max(self.size[1] - img.size[1], 0) / 2
+        return torchvision.transforms.functional.pad(img,
+                (math.floor(vertmargin), math.floor(horizmargin), math.ceil(vertmargin), math.ceil(horizmargin)), self.pad_with)
+        # return torchvision.transforms.functional.pad(img,
+        #         (0, 0, self.size[0] - img.size[0], self.size[1] - img.size[1]), self.pad_with)
+        # return PIL.ImageOps.expand(img, (0, 0, self.size[0] - img.size[0], self.size[1] - img.size[1]), self.pad_with)
+        # none of these are working ;-; let's try numpy
+    def __repr__(self):
+        return self.__class__.__name__ + f'(size={self.size} pad={self.pad_with})'
 
 # if running as main
 if __name__ == '__main__':
@@ -137,5 +170,9 @@ if __name__ == '__main__':
         print(fname)
     exit(0)
     dataset = OrientationImageFolder('./../iamdataset/words-sample')
-    dataset.make_dataset('./../iamdataset/words-sample', {'upright': 0, 'upside_down': 1})
+    dataset.make_dataset('./../iamdataset/words-sample', {'normal': 0, 'flipped': 1})
+
+
+# When creating a CNN for image classification, how many layers of each type (ie. convolutional, pooling, fully connected, etc.) is best? How deep should the layers be?
+
 
--- trocr.py
+++ trocr.py
@@ -6,7 +6,14 @@ import requests
 # url = 'https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg'
 # image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
 
-image = Image.open('iamdataset/words-sample/a01/a01-007/a01-007-00-00.png').convert("RGB")
+# image = Image.open('iamdataset/words-sample/a01/a01-007/a01-007-00-00.png').convert("RGB")
+
+path = 'iamdataset/sentences-sample/a01/a01-003/a01-003-s01-03.png'
+# path = 'iamdataset/forms-sample/a01-000u.png'
+image = Image.open(path).convert("RGB")
+
+# life peers into existence, they should not
+image = image.rotate(0) # you engage happy " aerospace open record of
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-handwritten')
 model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-small-handwritten')
 pixel_values = processor(images=image, return_tensors="pt").pixel_values
